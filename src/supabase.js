88d20ee3b647f2ede7c22f3eeb1671fbaece6c38
blkl-dot <bathelemydelpeyrou@gmail.com>
2026-05-31 16:10:45 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,9 @@
-import { createClient } from '@supabase/supabase-js' // URL déjà remplie ✅ (vérifie quand même dans Settings → API) const SUPABASE_URL = 'https://myipfprkixvgtlumyufq.supabase.co' // ⬇️ COLLE TA CLÉ "anon public" ICI, entre les guillemets ⬇️ const SUPABASE_KEY = 'sb_publishable_us52NuCiGfqJCrOAglgUxw_A5m83O-rI' export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+import { createClient } from '@supabase/supabase-js' 
+
+// URL déjà remplie ✅ (vérifie quand même dans Settings → API) 
+const SUPABASE_URL = 'https://myipfprkixvgtlumyufq.supabase.co' 
+
+// ⬇️ COLLE TA CLÉ "anon public" ICI, entre les guillemets ⬇️ 
+const SUPABASE_KEY = 'sb_publishable_us52NuCiGfqJCrOAglgUxw_A5m83O-rI' 
+  
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
