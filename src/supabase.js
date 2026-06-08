c84923eeafb3aa31a3a429b649c2c2e05bcb1922
blkl-dot <bathelemydelpeyrou@gmail.com>
2026-06-08 21:44:38 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'myipfprkixvgtlumyufq'
-
-const SUPABASE_KEY = 'sb_publishable_us52NuCiGfqJCrOAglgUxw_A5m83O-r'
+const SUPABASE_URL = 'https://myipfprkixvgtlumyufq.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_TA_CLE_ICI'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
